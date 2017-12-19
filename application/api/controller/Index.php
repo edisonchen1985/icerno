9bb5068b0f8902d44e9a52dbf0beff2a3663d20f
Edison Chen <edisonchen1985@gmail.com>
2017-12-19 08:41:26 +0800

    #local新增创建组和用户代码

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -13,6 +13,10 @@ https://github.com/Fab1en/rocket-chat-rest-client
 */
 class Index
 {
+    public function __construct(){
+        define('REST_API_ROOT', '/api/v1/');
+        define('ROCKET_CHAT_INSTANCE', 'http://localhost:3000');
+    }
 
     public function index()
     {
@@ -58,8 +62,7 @@ class Index
         $this->respond($respond_message,$room);
     }
     public function respond($respond_message,$room){
-        define('REST_API_ROOT', '/api/v1/');
-        define('ROCKET_CHAT_INSTANCE', 'http://localhost:3000');
+        
         // define('ROCKET_CHAT_INSTANCE', 'http://192.168.6.35:3000');
 
         // $api = new \RocketChat\Client();
@@ -82,7 +85,52 @@ class Index
 
     }
     public function createGroup(){
+        /*rocket-chat会调用此接口，并传过来json数据，格式为：
+            {"token":"wDkSSZB3tq4k69Kpi5MtHxTn","bot":false,"trigger_word":"ai","channel_id":"WWXkMNX2dRerYqmBY","channel_name":"test","message_id":"C6N39ZTuivcwYsnnp","timestamp":"2017-12-13T07:59:36.764Z","user_id":"SYEcdXMeBnEkdY5Qs","user_name":"edison","text":"i hahaha"}
+        */
+        $data = file_get_contents("php://input");
+        $data = json_decode($data,true);
 
+        
+        $info = $data['text'];
+        $userid = $data['user_id'];
+        //ai的账户
+        $rocket_login_username = 'ai1';
+        $rocket_login_password = 'ai1';
+        $admin = new \RocketChat\User($rocket_login_username, $rocket_login_password);
+
+        //edison的账户
+        $edison_login_username = 'edison';
+        $edison_login_password = '';
+        $edison = new \RocketChat\User($edison_login_username, $edison_login_password);
+        if( $admin->login() ) {
+            $room = 'ask-for-a-leave3';
+            // create a new channel
+            $channel = new \RocketChat\Channel( $room,array($admin,$edison));
+            // $channel->create();
+            $url = 'https://www.baidu.com';
+            $channel->postMessage($url);
+        }
+    }
+
+    public function createUser(){
+        $rocket_login_username = 'edison';
+        $rocket_login_password = 'edison';
+        $admin = new \RocketChat\User($rocket_login_username, $rocket_login_password);
+        if( $admin->login() ) {
+            // $rocket_login_username = 'ai1';
+            // $rocket_login_password = 'ai1';
+            // $fields = array();
+            // $fields['email'] = 'ai@qq.com';
+            // $fields['nickname'] = 'ai1';
+            // $new = new \RocketChat\User($rocket_login_username, $rocket_login_password,$fields);
+            // $result = $new->create();
+            // dump($result);
+            $client = new \RocketChat\Client();
+            $all = $client->list_users();
+            dump($all);
+        };
+        
     }
 }
 
