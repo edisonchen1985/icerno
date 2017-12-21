43cf0db7f5b57882268b36093ca1ea597ac49288
Edison Chen <edisonchen1985@gmail.com>
2017-12-21 14:42:14 +0800

    #分页和静态文件

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -10,8 +10,11 @@ class Index extends Controller
 {
 
     public function index(){
-        $result = Db::table('room_action_log')->order('log_time desc')->select();
+        $result = Db::table('room_action_log')->order('log_time desc')->paginate(10);
+        // 获取分页显示
+        $page = $result->render();
         $this->assign('lists',$result);
+        $this->assign('page',$page);
         return $this->fetch();
     }
         
