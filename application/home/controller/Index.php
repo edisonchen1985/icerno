d132cf347f1842920408e906c3997acdcb5db3a8
Edison Chen <edisonchen1985@gmail.com>
2018-01-04 20:45:19 +0800

    #file_management处理

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -1,12 +1,19 @@
 <?php
 namespace app\home\controller;
 use \think\Controller;
+use \think\Request;
 
 class Index extends Controller
 {
 
     public function index(){
         return $this->fetch();
+    }
+    public function file_management(){
+    	$roomId = Request::instance()->param('roomId',0);
+    	echo $roomId;exit;
+    	$this->assign('lists',$result);
+    	return $this->fetch();
     }
         
 }
