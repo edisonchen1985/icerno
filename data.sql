f84a79968ee9ce0feaac77e08ae3605ef2b11928
Edison Chen <edisonchen1985@gmail.com>
2018-01-08 21:13:57 +0800

    #

--- data.sql
+++ data.sql
@@ -9,3 +9,30 @@ CREATE TABLE `room_action_log` (
   `log_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '发生的时间',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='用户进出房间的日志记录表';
+
+
+#RocketChat文件和文件夹操作管理表
+CREATE TABLE `tm_room_names` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `channel_id` varchar(50) NOT NULL DEFAULT '' COMMENT 'TM频道id',
+  `create_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '创建的时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='对应于TM的房间ID存储表';
+
+#文件夹操作管理表
+CREATE TABLE `tm_folder_names` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '文件夹的名称',
+  `room_id` varchar(50) NOT NULL DEFAULT '' COMMENT '对应tm_room_names的主键id',
+  `create_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '创建的时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='文件夹管理存储表';
+
+#文件操作管理表
+CREATE TABLE `tm_file_names` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '文件的名称',
+  `folder_id` varchar(50) NOT NULL DEFAULT '' COMMENT '对应tm_folder_names的主键id',
+  `create_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '创建的时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='文件存储表';
